b9c5686802e0253e1ebab76073e27d940a31ae7a
Banti kumar <mr.bantikumar9716@gmail.com>
2022-04-24 15:24:42 +0530

    add

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import '../App.css'
 export const HomePage = () => {
   return (
     <div>
-        
+    <NavLink to={'/product'} className='homebtn'>Go to product</NavLink>
     <img className='image-container-home' src="https://www.carselonadaily.com/wp-content/uploads/2020/02/Tata-Altroz-Interior-Cars-With-The-Best-Interior-1200x900-1.jpeg" alt="" />
 
     </div>
